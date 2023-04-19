77741d7b7b6f43323cf447a0a2733e2eb4c9d279
santividela <santividela_11@hotmail.com>
2023-04-19 01:46:07 -0300

    finalizado-testeado

---
+++ src/main/java/com/geminus/challenge/service/PruebaService.java
@@ -0,0 +1,11 @@
+package com.geminus.challenge.service;
+
+
+public interface PruebaService {
+
+    public Double caso1(Integer nro);
+
+    public Integer caso2(int[] arreglo);
+
+    public void caso3(Integer cantidadJaulasPorGrupo, Integer cantidadDeGrupos);git
+}
